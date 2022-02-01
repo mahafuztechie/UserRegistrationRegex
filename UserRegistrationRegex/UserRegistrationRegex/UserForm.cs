7f495cdbb1a15e7375067bb61d6b77c128a65eb2
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-02-01 13:59:18 +0530

    [mahafuz] adding uc2 last name

--- UserRegistrationRegex/UserRegistrationRegex/UserForm.cs
+++ UserRegistrationRegex/UserRegistrationRegex/UserForm.cs
@@ -10,7 +10,8 @@ namespace UserRegistrationRegex
     internal class UserForm
     {
         public const string FIRST_NAME = "^[A-Z][a-zA-Z]{2}";
-       
+        public const string LAST_NAME = "^[A-Z][a-zA-Z]{2}";
+
         public  void ValidateFName(string fName) 
         {
             Regex regex = new Regex(FIRST_NAME);
@@ -25,6 +26,21 @@ namespace UserRegistrationRegex
             }
         
            
+        }
+        public void ValidateLName(string lName)
+        {
+            Regex regex = new Regex(LAST_NAME);
+
+            if (regex.IsMatch(lName))
+            {
+                Console.WriteLine("last name is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("last name does not match with pattern");
+            }
+
+
         }
     }
 }
